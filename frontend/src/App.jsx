a77b7c1b2ad91f6fe9714892ec3f990d8ba53bae
saloni pawar <salonipawar294@gmail.com>
2025-10-25 08:58:08 +0530

    Added frontend, backend, and blockchain setup with .gitignore

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-12xl font-bold text-red-500">Hello Saloni 🚀</h1>
+      <p className="font-bold text-fuchsia-500">l am salu</p>
+    </div>
+  );
+}
+
+export default App;
